f46705bda7c72c083c7450133c57b8c107d21b50
Artem <av.cherep@yandex.ru>
2023-06-16 18:35:02 +0300

    feat: добавить класс узла двусвязного списка

---
+++ src/ru/yandex/model/Node.java
@@ -0,0 +1,36 @@
+package ru.yandex.model;
+
+public class Node<T extends Task> {
+
+    private T item;
+    private Node<T> prev;
+    private Node<T> next;
+
+    public Node(T item) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public Node<T> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
