317e0be649577f8e0312926fdf01aa8de30e4362
Artem <av.cherep@yandex.ru>
2023-07-17 16:07:37 +0300

    refactor: добавить остальные файлы

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -15,28 +16,39 @@ public class Main {
     private static final Random random = new Random();
     private static final String PATH = "src/main/java/ru/yandex/resources/tasks.csv";
     private static TaskManager manager = Managers.getFileBackedManager(PATH);
-    private static final LocalDateTime START_TIME
+    private static final LocalDateTime START_TIME_1
             = LocalDateTime.of(2023, 7, 17, 10, 0);
+    private static final LocalDateTime START_TIME_2
+            = LocalDateTime.of(2023, 7, 17, 12, 0);
+    private static final LocalDateTime START_TIME_3
+            = LocalDateTime.of(2023, 7, 17, 13, 0);
 
     public static void main(String[] args) {
-        Task task1 = new Task(START_TIME, 20)
+        Task task1 = new Task(START_TIME_1, 20)
                 .setId(random.nextInt())
                 .setName("Task")
                 .setStatus(Status.NEW)
                 .setDescription("Task description");
         manager.addTask(task1);
+        Task task2 = new Task(START_TIME_2, 100)
+                .setId(task1.getId())
+                .setName("Task")
+                .setStatus(Status.NEW)
+                .setDescription("Task description");
+        manager.deleteTask(task1.getId());
 
-        Epic epic1 = (Epic) new Epic()
+        Epic epic1 = (Epic) new Epic(START_TIME_2, 30)
                 .setId(random.nextInt())
                 .setName("Epic")
                 .setStatus(Status.NEW)
                 .setDescription("Epic description");
         manager.addEpic(epic1);
-        Subtask epic1subtask1 = (Subtask) new Subtask()
+        Subtask epic1subtask1 = (Subtask) new Subtask(START_TIME_3, 40)
                 .setId(random.nextInt())
                 .setName("Subtask")
                 .setStatus(Status.NEW)
-                .setDescription("Subtask description");
+                .setDescription("Subtask description")
+                .setDuration(Duration.ofMinutes(20));
         epic1subtask1.setEpicId(epic1.getId());
         manager.getEpic(epic1.getId());
         manager.getTask(task1.getId());
@@ -49,5 +61,9 @@ public class Main {
             System.out.println(e.getMessage());
         }
         manager = FileBackedTaskManager.loadFromFile(new File(PATH));
+        List<Task> priority = manager.getPrioritizedTasks();
+        for (Task task : priority) {
+            System.out.println(task);
+        }
     }
 }
