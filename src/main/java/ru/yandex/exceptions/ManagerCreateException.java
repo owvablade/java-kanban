f76d8800297e6e351bba769d4f38765a44b34220
Artem <av.cherep@yandex.ru>
2023-07-30 12:04:23 +0300

    feat: добавить новый исключения для отображения ошибки при создании HttpTaskManager

---
+++ src/main/java/ru/yandex/exceptions/ManagerCreateException.java
@@ -0,0 +1,8 @@
+package ru.yandex.exceptions;
+
+public class ManagerCreateException extends RuntimeException {
+
+    public ManagerCreateException(final String message) {
+        super(message);
+    }
+}
